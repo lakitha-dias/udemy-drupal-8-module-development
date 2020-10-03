1a6477d64b4ff993e839c37b9e59e6158cf5bcbf
lakitha-dias <lakitha.dias@gmail.com>
2020-10-03 20:56:23 +0530

    11. Renderable array example in block

--- module_hero/src/Plugin/Block/HelloBlock.php
+++ module_hero/src/Plugin/Block/HelloBlock.php
@@ -20,9 +20,39 @@ class HelloBlock extends BlockBase{
      */
     
     public function build(){
-        return [
-            '#type' => 'markup',
-            '#markup' => $this->t('The output of our superheros block'),
+        
+        
+        $heroes = [
+            ['hero_name' => 'Hulk','real_name' => 'david'],
+            ['hero_name' => 'Thor' ,'real_name' => 'david'],
+            ['hero_name' => 'Iron Man' , 'real_name' => 'david'],
+            ['hero_name' => 'Luck Cage' , 'real_name' => 'david'],
+            ['hero_name' => 'Black widow' , 'real_name' => 'david'],
+            ['hero_name' => 'Daredevil' , 'real_name' => 'david'],
+            ['hero_name' => 'Captain America' , 'real_name' => 'david'],
+            ['hero_name' => 'Wolverine' , 'real_name' => 'david'],
         ];
+        
+        $table =  [
+            '#type' => 'table',
+            '#header' => [
+                $this->t('real name')
+            ]
+        ];
+        
+        foreach($heroes as $hero){
+            $table[] = [
+                'hero_name' =>  [
+                    '#type' => 'markup',
+                    '#markup' => $hero['hero_name'],
+                ],
+                'real_name' => [
+                    '#type' => 'markup',
+                    '#markup' => $hero['real_name'],
+                ],
+            ];
+        }
+        
+        return $table;
     }
 }
