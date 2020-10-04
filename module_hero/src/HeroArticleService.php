7796d8e2163276d6bde98e950baf535311c800eb
lakitha-dias <lakitha.dias@gmail.com>
2020-10-04 20:21:42 +0530

    14. Services injection example

--- module_hero/src/HeroArticleService.php
+++ module_hero/src/HeroArticleService.php
@@ -1,19 +1,36 @@
 <?php
 
+namespace Drupal\module_hero;
+use Drupal\Core\Entity\Query\QueryFactory; 
+use Drupal\Core\Entity\EntityManager;
 
 /**
  * 
- * @author Lakitha
+ * @author Lakitha Dias
  *
  */
 
 class HeroArticleService {
     
+    private $entityQuery;
+    private $entityManager;
+    
+    public function __construct(QueryFactory $queryFactory, EntityManager $entityManager){
+        $this->queryFactory = $queryFactory;
+        $this->entityManager =  $entityManager;
+    }
+    
     /**
      * Method for getting articles
      */
     public function getHeroArticles(){
+        
         $articles = ['hulk is green', 'Flash is red'];
+        
+        $articleNids = $this->entityQuery->get('node')->condition('type','article')->execute();
+        
+        kint($this->entityManager->getStorage('node')->loadMultiple());
+        
         return $articles;
     }
 }
