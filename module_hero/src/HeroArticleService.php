d2abd82decc99da1c7edb791f76bef9e012fb56c
lakitha-dias <lakitha.dias@gmail.com>
2020-10-04 20:05:36 +0530

    13. Custom service

---
+++ module_hero/src/HeroArticleService.php
@@ -0,0 +1,19 @@
+<?php
+
+
+/**
+ * 
+ * @author Lakitha
+ *
+ */
+
+class HeroArticleService {
+    
+    /**
+     * Method for getting articles
+     */
+    public function getHeroArticles(){
+        $articles = ['hulk is green', 'Flash is red'];
+        return $articles;
+    }
+}
